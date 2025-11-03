8c7759063c13e0c77ee3255c968a57e24db9c4cc
Gantumur Battumur <gantumurq@gmail.com>
2025-11-03 15:29:17 -0700

    db credentials fixed

--- backend/database.py
+++ backend/database.py
@@ -2,13 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./memorypalace.db"
+import os
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+DATABASE_URL = f"postgresql://postgres:{os.getenv('SUPABASE_PASSWORD')}@db.sgygkspqnstkenbjuawl.supabase.co:5432/postgres"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 # Dependency for FastAPI
